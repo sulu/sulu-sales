8facddc0f6e2f95001236df4a6a94a5ffce18f5f
Michael Zangerle <michael.zangerle@massiveart.com>
2014-09-23 08:59:07 +0200

    worked on integration of flow of documents

---
+++ Widgets/FlowOfDocuments.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\Sales\CoreBundle\Widgets;
+
+use Sulu\Bundle\AdminBundle\Widgets\WidgetException;
+use Sulu\Bundle\AdminBundle\Widgets\WidgetInterface;
+
+/**
+ * Class FlowOfDocuments
+ * Abstract class for widgets which show flow of documents (invoice, order, etc)
+ *
+ * @package Sulu\Bundle\Sales\CoreBundle\Widgets
+ */
+abstract class FlowOfDocuments implements WidgetInterface
+{
+    private $entries = [];
+
+    /**
+     * @return \Sulu\Bundle\Sales\CoreBundle\Core\SalesDocument[]
+     */
+    public function getEntries()
+    {
+        return $this->entries;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function addEntry(array $data)
+    {
+        $this->entries[] = $data;
+    }
+
+    /**
+     * Sorts the data array by the date
+     * @param bool $desc
+     */
+    protected function orderDataByDate($desc = true)
+    {
+        usort(
+            $this->entries,
+            function ($a, $b) use ($desc) {
+                if ($a['date'] > $b['date']) {
+                    if($desc) {
+                        return 1;
+                    }
+                    return -1;
+                } elseif ($a['date'] < $b['date']) {
+                    if(!$desc){
+                        return -1;
+                    }
+                    return 1;
+                } else {
+                    return 0;
+
+                }
+            }
+        );
+    }
+
+    /**
+     * returns template name of widget
+     *
+     * @return string
+     */
+    public function getTemplate()
+    {
+        return 'SuluSalesCoreBundle:Widgets:core.account.html.twig';
+    }
+}
