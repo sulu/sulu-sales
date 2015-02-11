99c911ebb2de851bf5734a6fc87a0b881c3b0fbb
Michael Zangerle <michael.zangerle@massiveart.com>
2015-02-11 10:05:50 +0100

    removed old code

--- Tests/app/AppKernel.php
+++ Tests/app/AppKernel.php
@@ -8,31 +8,6 @@ class AppKernel extends SuluTestKernel
     public function registerBundles()
     {
         $bundles = parent::registerBundles();
-//        $bundles = array(
-//            // Dependencies
-//            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-//            new Symfony\Bundle\TwigBundle\TwigBundle(),
-//            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-//            new Symfony\Bundle\MonologBundle\MonologBundle(),
-//            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-//            new JMS\SerializerBundle\JMSSerializerBundle(),
-//            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-//            new FOS\RestBundle\FOSRestBundle(),
-//            new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
-//            new \Liip\ThemeBundle\LiipThemeBundle(),
-//
-//            // Sulu
-//            new \Sulu\Bundle\CoreBundle\SuluCoreBundle(),
-//            new \Sulu\Bundle\AdminBundle\SuluAdminBundle(),
-//            new \Sulu\Bundle\ProductBundle\SuluProductBundle(),
-//            new \Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
-//            new \Sulu\Bundle\TestBundle\SuluTestBundle(),
-//            new \Sulu\Bundle\ContactBundle\SuluContactBundle(),
-//            new \Sulu\Bundle\TagBundle\SuluTagBundle(),
-//            new \Sulu\Bundle\Sales\CoreBundle\SuluSalesCoreBundle(),
-//            new \Sulu\Bundle\WebsiteBundle\SuluWebsiteBundle(),
-//        );
-
         return $bundles;
     }
 
