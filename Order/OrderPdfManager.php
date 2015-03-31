cc49ba8364cdacdb5ce6990a09514454a8f90361
turbo-ele <eliash@gmx.at>
2015-03-31 11:31:56 +0200

    outsourced pdf generation to its own bundle

---
+++ Order/OrderPdfManager.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Sulu\Bundle\Sales\OrderBundle\Order;
+
+use Massive\Bundle\PdfBundle\Pdf\PdfManager;
+use \Sulu\Bundle\Sales\OrderBundle\Api\Order as ApiOrder;
+
+class OrderPdfManager
+{
+    /**
+     * @var string
+     */
+    protected $websiteLocale;
+
+    /**
+     * @var PdfManager
+     */
+    protected $pdfManager;
+
+    /**
+     * @param PdfManager $pdfManager
+     * @param $websiteLocale
+     */
+    public function __construct(
+        PdfManager $pdfManager,
+        $websiteLocale
+    )
+    {
+        $this->pdfManager = $pdfManager;
+        $this->websiteLocale = $websiteLocale;
+    }
+
+    /**
+     * @param $order
+     * @return string
+     */
+    public function getPdfName($order)
+    {
+        $pdfName = 'PA_OrderConfirmation-' . $order->getNumber() . '.pdf';
+
+        return $pdfName;
+    }
+
+    /**
+     * @param ApiOrder $apiOrder
+     * @param $baseUrl
+     * @return file
+     */
+    public function createOrderConfirmation(ApiOrder $apiOrder, $baseUrl)
+    {
+        $order = $apiOrder->getEntity();
+
+        $data = array(
+            'baseUrl' => $baseUrl,
+            'recipient' => $order->getDeliveryAddress(),
+            'responsibleContact' => $order->getResponsibleContact(),
+            'deliveryAddress' => $order->getInvoiceAddress(),
+            'order' => $order,
+            'orderApiEntity' => $apiOrder,
+            'itemApiEntities' => $apiOrder->getItems(),
+            'website_locale' => $this->websiteLocale
+        );
+
+        $header = $this->pdfManager->renderTemplate(
+            'PoolAlpinBaseBundle:Default:pdf-base-header.html.twig',
+            array(
+                'baseUrl' => $baseUrl,
+            )
+        );
+
+        $footer = $this->pdfManager->renderTemplate(
+            'PoolAlpinBaseBundle:Default:pdf-base-footer.html.twig',
+            array(
+                'baseUrl' => $baseUrl,
+            )
+        );
+
+        $pdf = $this->pdfManager->convertToPdf(
+            'SuluSalesOrderBundle:Template:order.confirmation.pdf.html.twig',
+            $data,
+            false,
+            array(
+                'footer-html' => $footer,
+                'header-html' => $header
+            )
+        );
+
+        return $pdf;
+    }
+}
