9433f839757dc747dbc8cda670867243d1b1f818
turbo-ele <eliash@gmx.at>
2015-05-28 18:08:09 +0200

    adapted pdf templates for inheritance

--- Order/OrderPdfManager.php
+++ Order/OrderPdfManager.php
@@ -9,6 +9,7 @@
  */
 namespace Sulu\Bundle\Sales\OrderBundle\Order;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Massive\Bundle\PdfBundle\Pdf\PdfManager;
 use Sulu\Bundle\Sales\OrderBundle\Api\ApiOrderInterface;
 
@@ -40,23 +41,39 @@ class OrderPdfManager
     protected $templateMacrosPath;
 
     /**
+     * @var string
+     */
+    protected $templateConfirmationPath;
+
+    /**
+     * @var ObjectManager
+     */
+    protected $entityManager;
+
+    /**
+     * @param ObjectManager $entityManager
      * @param PdfManager $pdfManager
+     * @param string $templateConfirmationPath
      * @param string $templateBasePath
      * @param string $templateHeaderPath
      * @param string $templateFooterPath
      * @param string $templateMacrosPath
      */
     public function __construct(
+        ObjectManager $entityManager,
         PdfManager $pdfManager,
+        $templateConfirmationPath,
         $templateBasePath,
         $templateHeaderPath,
         $templateFooterPath,
         $templateMacrosPath
     ) {
+        $this->entityManager = $entityManager;
         $this->pdfManager = $pdfManager;
+        $this->templateConfirmationPath = $templateConfirmationPath;
+        $this->templateBasePath = $templateBasePath;
         $this->templateHeaderPath = $templateHeaderPath;
         $this->templateFooterPath = $templateFooterPath;
-        $this->templateBasePath = $templateBasePath;
         $this->templateMacrosPath = $templateMacrosPath;
     }
 
@@ -79,6 +96,40 @@ class OrderPdfManager
      */
     public function createOrderConfirmation(ApiOrderInterface $apiOrder)
     {
+        $data = $this->getContentForPdf($apiOrder);
+
+        $header = $this->pdfManager->renderTemplate(
+            $this->templateHeaderPath,
+            array()
+        );
+
+        $footer = $this->pdfManager->renderTemplate(
+            $this->templateFooterPath,
+            array()
+        );
+
+        $pdf = $this->pdfManager->convertToPdf(
+            $this->templateConfirmationPath,
+            $data,
+            false,
+            array(
+                'footer-html' => $footer,
+                'header-html' => $header
+            )
+        );
+
+        return $pdf;
+    }
+
+    /**
+     * Function that sets data array for pdf rendering
+     *
+     * @param ApiOrderInterface $apiOrder
+     *
+     * @return array
+     */
+    protected function getContentForPdf(ApiOrderInterface $apiOrder)
+    {
         $order = $apiOrder->getEntity();
 
         $data = array(
@@ -92,26 +143,6 @@ class OrderPdfManager
             'templateMacrosPath' => $this->templateMacrosPath,
         );
 
-        $header = $this->pdfManager->renderTemplate(
-            $this->templateHeaderPath,
-            array()
-        );
-
-        $footer = $this->pdfManager->renderTemplate(
-            $this->templateFooterPath,
-            array()
-        );
-
-        $pdf = $this->pdfManager->convertToPdf(
-            'SuluSalesOrderBundle:Template:order.confirmation.pdf.html.twig',
-            $data,
-            false,
-            array(
-                'footer-html' => $footer,
-                'header-html' => $header
-            )
-        );
-
-        return $pdf;
+        return $data;
     }
 }
