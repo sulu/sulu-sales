d3086b44a668676b6079d6adc798f01128bbbbf2
Patrick Hopfner <patrick.hopfner@massiveart.com>
2016-03-24 15:15:38 +0100

    Fixed doc

--- src/Sulu/Bundle/Sales/ShippingBundle/Behat/ShippingContext.php
+++ src/Sulu/Bundle/Sales/ShippingBundle/Behat/ShippingContext.php
@@ -6,7 +6,7 @@ use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Sulu\Bundle\TestBundle\Behat\DefaultContext;
 
 /**
- * Behat context class for the InquiryBundle.
+ * Behat context class for the ShippingBundle.
  */
 class ShippingContext extends DefaultContext
 {
